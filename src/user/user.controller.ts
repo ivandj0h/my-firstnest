219b2ac8e01c8ea9125a3177ea38aa331c1a88a4
ivandjoh <ivandi.djoh@gmail.com>
2023-06-18 19:46:13 +0800

    controller adjustment

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
 
+    // This Method will be called when we hit the route /user/all
     @Get("all")
     findAll(): string {
         return "This action returns all users";
     };
+
+    // This Method will be called when we hit the route /user/1
+    @Get(":id")
+    findOne(@Param('id') id:string) {
+        return ({
+            user: {
+                id: id,
+            }
+        })
+    }
+
+    // This Method will be called when we hit the route /user
+    @Post()
+    create(@Body() body:any) {
+        return body;
+    }
 }
